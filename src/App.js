15a50144fca36ea2a76aaf65f5f68954fc33c079
mitchellss <stephen.mitchell2299@gmail.com>
2020-10-15 18:20:28 +0000

    add super basic routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
