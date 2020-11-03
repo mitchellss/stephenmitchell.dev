c52b966687989518ea663dcad589987c2e358116
mitchellss <stephen.mitchell2299@gmail.com>
2020-11-03 02:37:35 +0000

    move research to projects

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from './Components/Home';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Projects from './Components/Projects';
-import Research from './Components/Research';
 import Contact from './Components/Contact';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -23,7 +22,6 @@ function App() {
           <Route path="/about" exact component={About} />
           <Route path="/resume" exact component={Resume} />
           <Route path="/projects" exact component={Projects} />
-          <Route path="/research" exact component={Research} />
           <Route path="/contact" exact component={Contact} />
         </Switch>
       </Router>
